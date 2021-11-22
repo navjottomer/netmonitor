a9f16132b7fdaf07f02a0221a3d84fdaf00f7eb4
navjottomer <navjottomer@gmail.com>
2021-11-22 13:40:36 +0530

    gitignore and build script update

---
+++ github/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+# Automatically build a zip file with version string from manifest.json
+#------------------------------------------------------------------------------
+# change to root of project
+cd "$(dirname "$0")/.."
+# First check if dist folder exists and if not create it
+if [ ! -d "dist" ]; then
+  mkdir dist
+fi
+# build a zip file with version name from manifest.json
+echo "Building zip file..."
+# read the version from manifest.json
+version=$(grep -m 1 '"version"' manifest.json | cut -d '"' -f 4)
+# remove existing zip file if it exists
+if [ -f "dist/netmonitor-v$version.zip" ]; then
+  rm "dist/netmonitor-v$version.zip"
+fi
+# create a zip file with the version name excluding github folder
+zip -r dist/netmonitor-v$version.zip . -x "*dist*" "*.git*" "*github*" "*.idea*" "*.iml*" "*.md" "*.gitignore"
+echo "Zip file created."
+#------------------------------------------------------------------------------
+# EOF
